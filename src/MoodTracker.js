26268efeed586a1b71bf85ed335097b60c3733be
blangwell <b.langwell@outlook.com>
2021-02-02 14:10:47 -0800

    Add increaseMood class method

--- src/MoodTracker.js
+++ src/MoodTracker.js
@@ -6,12 +6,19 @@ class MoodTracker extends Component {
     this.state = {
       moodPoints: 1
     };
-  }
+  };
+  increaseMood = () => {
+    let newMoodPoints = this.state.moodPoints >= 10 ? 0 : this.state.moodPoints + 1;
+    this.setState({
+      moodPoints: newMoodPoints
+    });
+  };
   render() {
     return (
       <div>
         <h3>On a scale from 1 - 10,</h3>
         <p>You are this happy: {this.state.moodPoints}</p>
+        <button onClick={this.increaseMood}>Add Point</button>
       </div>
     )
   }
